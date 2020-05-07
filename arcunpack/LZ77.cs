7f8780d696108dffe67a415dc7d3c6aa5a88fabb
Mike Calfin <mcalfin28@gmail.com>
2020-05-07 07:12:15 -0700

    initial commit

---
+++ arcunpack/LZ77.cs
@@ -0,0 +1,472 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace arcunpack
+{
+    class LZDecompress
+    {
+        private bool eof = false;
+        private byte[] ring = new byte[0x1000];
+        private int pos = 0;
+        private int copyPos = 0;
+        private int copyLen = 0;
+        private int flags = 1;
+        Stream input;
+
+        public LZDecompress(byte[] data)
+        {
+            input = new MemoryStream(data);
+        }
+
+        public int read()
+        {
+            int b;
+            int flag;
+
+            if (eof)
+            {
+                return -1;
+            }
+
+            if (copyLen > 0)
+            {
+                b = ring[copyPos] & 0xFF;
+
+                copyPos = (copyPos + 1) & 0xFFF;
+                copyLen--;
+            }
+            else
+            {
+                flag = nextFlag();
+
+                if (flag == 1)
+                {
+                    b = input.ReadByte();
+                }
+                else if (flag == 0)
+                {
+                    b = readBackRef();
+                }
+                else
+                {
+                    b = -1;
+                }
+            }
+
+            if (b != -1)
+            {
+                ring[pos] = (byte)b;
+                pos = (pos + 1) & 0xFFF;
+
+                return b;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+
+        private int nextFlag()
+        {
+            if (flags == 1)
+            {
+                int r = input.ReadByte();
+
+                if (r == -1)
+                {
+                    return -1;
+                }
+                else
+                {
+                    flags = 0x100 | r;
+                }
+            }
+
+            int flag = flags & 1;
+            flags = flags >> 1;
+
+            return flag;
+        }
+
+        private int readBackRef()
+        {
+            int hi = input.ReadByte();
+
+            if (hi == -1)
+            {
+                return -1;
+            }
+
+            int lo = input.ReadByte();
+
+            if (hi == -1)
+            {
+                throw new IOException("Unexpected EOF mid-backref");
+            }
+
+            copyLen = (lo & 0x0F);
+            copyPos = (hi << 4) | (lo >> 4);
+
+            if (copyPos > 0)
+            {
+                copyLen += 3;
+                copyPos = (pos - copyPos) & 0xFFF;
+
+                int b = ring[copyPos] & 0xFF;
+
+                copyPos = (copyPos + 1) & 0xFFF;
+                copyLen--;
+
+                return b;
+            }
+            else
+            {
+                eof = true;
+
+                return -1;
+            }
+        }
+
+
+        public int read(byte[] b, int off, int len)
+        {
+            int i;
+
+            for (i = 0; i < len; i++)
+            {
+                int r = read();
+
+                if (r != -1)
+                {
+                    b[off + i] = (byte)r;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return i;
+        }
+
+        public int read(byte[] b)
+        {
+            return read(b, 0, b.Count());
+        }
+
+        public long skip(long n)
+        {
+            long i;
+
+            for (i = 0; i < n; i++)
+            {
+                if (read() == -1)
+                {
+                    break;
+                }
+            }
+
+            return i;
+        }
+
+        public byte[] decompress()
+        {
+            MemoryStream outBytes = new MemoryStream();
+
+            while (true)
+            {
+                int oByte = read();
+                if (oByte == -1)
+                    break;
+                outBytes.WriteByte(Convert.ToByte(oByte));
+            }
+            return outBytes.ToArray();
+        }
+    }
+
+
+    class LZCompress
+    {
+        private byte[] ring = new byte[0x1000];
+
+        private static int MIN_MATCH = 3;
+        private static int MAX_MATCH = MIN_MATCH + 15;
+        private static short NULL = -1;
+
+        private short[] links = new short[0x1000];
+        private short[] heads = new short[0x100];
+        private short[] tails = new short[0x100];
+        private short[] cursors = new short[0x1000];
+
+        private byte[] match = new byte[MAX_MATCH];
+        private byte[] packet = new byte[16 * 2];
+
+        private int ringPos = 0;
+        private int ncursors = 0;
+        private int nmatched = 0;
+        private int pktPos = 0;
+        private int flags = 0x10000;
+        Stream output;
+
+        public LZCompress()
+        {
+            output = new MemoryStream();
+
+            for (int i = 0; i < 0x100; i++)
+            {
+                heads[i] = NULL;
+                tails[i] = NULL;
+            }
+        }
+
+        public Stream outputdata()
+        {
+            return output;
+        }
+
+        public void close()
+        {
+
+            emitMatch();
+
+            // We need to send a <0, 0> backref here to indicate end of file.
+
+            flags = (flags >> 1);
+            packet[pktPos++] = 0;
+            packet[pktPos++] = 0;
+
+            // Bitshift until all the flag bits are in the right place.
+            // Every flag to the left of the zero flag for the magic backref
+            // is irrelevant, so we may as well make those zeros.
+
+            while ((flags & 0x100) == 0)
+            {
+                if (flags == 0)
+                {
+                    Console.WriteLine("Error!");
+                }
+
+                flags = (flags >> 1);
+            }
+
+            // Write out our fancy final packet and close up.
+
+            output.WriteByte((byte)(flags & 0xFF));
+            output.Write(packet, 0, pktPos);
+
+            output.Flush();
+
+        }
+
+        public void write(byte[] b, int off, int len)
+        {
+            for (int i = 0; i < len; i++)
+            {
+                write(b[off + i] & 0xFF);
+            }
+        }
+
+
+        public void write(byte[] b)
+        {
+            write(b, 0, b.Length);
+        }
+
+        public void write(int b)
+        {
+            if (b < 0 || b > 255)
+            {
+                return;
+            }
+            if (nmatched == 0)
+            {
+
+                initCursors(b);
+            }
+            else if (nmatched == MAX_MATCH)
+            {
+                emitMatch();
+                initCursors(b);
+            }
+            else
+            {
+                updateCursors(b);
+            }
+
+            advance(b);
+        }
+
+        private void advance(int newValue)
+        {
+            // Get previous symbol at the current ring position, remove it from
+            // the head of that symbol's occurrence list
+            int oldValue = ring[ringPos] & 0xFF;
+            short oldHead = heads[oldValue];
+
+            if (oldHead != NULL)
+            {
+                short newHead = links[oldHead];
+
+                if (newHead == NULL)
+                {
+                    tails[oldValue] = NULL;
+                }
+
+                heads[oldValue] = newHead;
+            }
+
+            // Insert new symbol (at the current ring position) at the tail of
+            // the new symbol's occurrence list
+            short oldTail = tails[newValue];
+            short newTail = (short)ringPos;
+
+            if (oldTail == NULL)
+            {
+                heads[newValue] = newTail;
+            }
+            else
+            {
+                links[oldTail] = newTail;
+            }
+
+            tails[newValue] = newTail;
+            links[newTail] = NULL;
+
+            // Finally, the easy part: update the ring and advance our position.
+            ring[ringPos] = (byte)newValue;
+            ringPos = (ringPos + 1) & 0xFFF;
+        }
+
+        private void initCursors(int b)
+        {
+            ncursors = 0;
+
+            for (short pos = heads[b]; pos != NULL; pos = links[pos])
+            {
+                /*
+                 * This SHOULD (and in fact DOES) work but Konami made
+                 * distance=0 mean End of File for some bone head fucking
+                 * reason. This is also why close() is nontrivial (because you
+                 * NEED to send an EOF marker or their decompressor throws a
+                 * shit fit).
+                 *
+                 * Basically the stuff inside the 'if' should always work but
+                 * we have to filter this particular case out.
+                 */
+                if (pos != (ringPos & 0xFFF))
+                {
+                    cursors[ncursors++] = pos;
+                }
+            }
+
+            if (ncursors > 0)
+            {
+                match[nmatched++] = (byte)b;
+            }
+            else
+            {
+                pushVerbatim((byte)b);
+            }
+        }
+
+        private void updateCursors(int b)
+        {
+            int cursorNo = 0;
+
+            while (cursorNo < ncursors)
+            {
+                int pos = (cursors[cursorNo] + nmatched) & 0xFFF;
+
+                if ((ring[pos] & 0xFF) != b)
+                {
+                    if (ncursors > 1)
+                    {
+                        // Kill this cursor and stay put (so we process whatever
+                        // cursor has moved into the evicted cursor's slot in the
+                        // next iteration)
+                        cursors[cursorNo] = cursors[--ncursors];
+                    }
+                    else
+                    {
+                        // Last remaining match, emit this and restart with new symbol
+                        // That restart is a subtle point that took me a while to remember! :)
+                        emitMatch();
+                        initCursors(b);
+
+                        return;
+                    }
+                }
+                else
+                {
+                    // Move to next cursor slot
+                    cursorNo++;
+                }
+            }
+
+            match[nmatched++] = (byte)b;
+        }
+
+
+        private void pushVerbatim(byte b)
+        {
+            packet[pktPos++] = b;
+            flags = (flags >> 1) | 0x80;
+
+            if ((flags & 0x100) != 0)
+            {
+                writePacket();
+            }
+        }
+
+        private void emitMatch()
+        {
+            if (nmatched < MIN_MATCH)
+            {
+                // Below break-even point, emit verbatim
+                for (int i = 0; i < nmatched; i++)
+                {
+                    pushVerbatim(match[i]);
+                }
+            }
+            else
+            {
+                // Above break-even, emit backref.
+                // Note that at this stage, either only one match remains, or all
+                // matches exceed the max match length (so we may as well use the
+                // cursor in the first slot anyway)
+                flags = (flags >> 1);
+
+                int matchStartPos = (ringPos - nmatched) & 0xFFF;
+                int matchOffset = (matchStartPos - cursors[0]) & 0xFFF;
+
+                packet[pktPos++] = (byte)(matchOffset >> 4);
+                packet[pktPos++] = (byte)(((matchOffset & 0x0F) << 4) | (nmatched - MIN_MATCH));
+
+                if ((flags & 0x100) != 0)
+                {
+                    writePacket();
+                }
+            }
+
+            nmatched = 0;
+        }
+        private void writePacket()
+        {
+
+            output.WriteByte((byte)(flags & 0xFF));
+            output.Write(packet, 0, pktPos);
+
+            pktPos = 0;
+            flags = 0x10000;
+        }
+
+
+    }
+
+}
+
+
---
+++ arcunpack/Program.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace arcunpack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            Console.WriteLine("arc utils, v0.3");
+            if(args.Length == 0)
+            {
+                Console.WriteLine("extract: {0} <x> <filename>", System.AppDomain.CurrentDomain.FriendlyName);
+                Console.WriteLine("pack: {0} <p> <filename> <dir to pack>", System.AppDomain.CurrentDomain.FriendlyName);
+            }
+            else if (args[0] == "x")
+            {
+                //Parse header
+                //byte[] filedata = File.ReadAllBytes(args[0]);
+                BinaryReader b = new BinaryReader(File.Open(args[1], FileMode.Open));
+                byte[] header = b.ReadBytes(4);
+                if (header[0] != 0x20 || header[1] != 0x11 || header[2] != 0x75 || header[3] != 0x19)
+                {
+                    Console.WriteLine("Invalid header - not arc file");
+                    return;
+                }
+                int unkFlag1 = b.ReadInt32();
+                int numFiles = b.ReadInt32();
+                int unkFlag2 = b.ReadInt32();
+
+                Console.WriteLine("UnkFlag1: " + unkFlag1);
+                Console.WriteLine("Number of Files: " + numFiles);
+                Console.WriteLine("UnkFlag2: " + unkFlag2);
+
+                for (int i = 0; i < numFiles; i++)
+                {
+                    int fileStrOffset = b.ReadInt32();
+                    int fileDataOffset = b.ReadInt32();
+                    int fileDataDecomSize = b.ReadInt32();
+                    int fileDataLen = b.ReadInt32();
+
+                    long curPos = b.BaseStream.Position;
+
+                    b.BaseStream.Position = fileStrOffset;
+                    string fileName = ReadAscii(b);
+                    Console.WriteLine("**************");
+                    Console.WriteLine("File Name: " + fileName);
+                    Console.WriteLine("File Data Decompressed Size: " + fileDataDecomSize);
+                    Console.WriteLine("File Data Raw Offset: " + fileDataOffset);
+                    Console.WriteLine("File Data Raw Length: " + fileDataLen);                    
+
+                    bool isCompressed = false;
+
+                    if (fileDataLen != fileDataDecomSize)
+                    {
+                        isCompressed = true;
+                    }
+
+                    b.BaseStream.Position = fileDataOffset;
+
+                    byte[] fileBytes;
+                    byte[] fileCompressedBytes = b.ReadBytes(fileDataLen);
+
+                    if (isCompressed)
+                    {
+                        LZDecompress lz = new LZDecompress(fileCompressedBytes);
+                        
+                        fileBytes = lz.decompress();
+                    }
+                    else
+                        fileBytes = fileCompressedBytes;
+
+                    FileInfo file = new FileInfo(fileName);
+                    file.Directory.Create();
+
+                    File.WriteAllBytes(fileName, fileBytes);
+
+
+                    b.BaseStream.Position = curPos;
+                }
+            }
+            else if(args[0]=="p")
+            {
+                string pathToPack = args[2];
+                string arcFileName = args[1];
+                bool compression = true;
+                if (args.Contains("--no-compression"))
+                {
+                    compression = false;
+                }
+                
+
+
+                
+                Console.WriteLine("Processing: " + pathToPack+" into "+arcFileName);
+
+                DirectoryInfo rootdir = new DirectoryInfo(pathToPack);
+
+                WalkDirectory(rootdir, arcFileName, pathToPack, compression);
+
+
+
+                //Write header
+                //Write unk 1
+                //Write num files
+                //Write unk 2
+                //Write file offsets (file str offset, 
+                //Write strings
+                //Write file data
+                FileStream output = new FileStream(arcFileName, FileMode.Create);
+                output.WriteByte(0x20); //header
+                output.WriteByte(0x11);
+                output.WriteByte(0x75);
+                output.WriteByte(0x19);
+                output.Write(BitConverter.GetBytes(1), 0, 4); //unk
+                output.Write(BitConverter.GetBytes(filesToPack), 0, 4);
+                output.Write(BitConverter.GetBytes(2), 0, 4); //unk
+
+                //table of contents - write blanks, we'll fill this in later
+
+                long[] fStructOffsets = new long[filesToPack];
+
+                for (int i = 0; i < filesToPack; i++)
+               {
+                    fStructOffsets[i] = output.Position;
+                    output.Write(BitConverter.GetBytes(0), 0, 4); //String offset
+                    output.Write(BitConverter.GetBytes(0), 0, 4); //Data offset
+                    output.Write(BitConverter.GetBytes(decFileSizes[i]), 0, 4); //Uncompressed size
+                    output.Write(BitConverter.GetBytes(compFileSizes[i]), 0, 4); //Compressed size
+                }
+
+                long[] fNameOffsets = new long[filesToPack];
+
+                
+
+                //file names
+                for (int i = 0; i < filesToPack; i++)
+                {
+                    fNameOffsets[i] = output.Position;
+                    fileNames[i] = fileNames[i].Replace('\\', '/');
+                    byte[] toBytes = Encoding.ASCII.GetBytes(fileNames[i]+ "\0");
+                    
+                    output.Write(toBytes, 0, toBytes.Length);
+                }
+
+                long[] fDataOffsets = new long[filesToPack];
+
+                //file data
+                for(int i=0;i<filesToPack;i++)
+                {
+                    fDataOffsets[i] = output.Position;                    
+                    byte[] filebytes = fileData[i].ToArray();
+                    output.Write(filebytes, 0, filebytes.Length);
+                }
+
+                //go back and write file offsets
+                for(int i=0;i<filesToPack;i++)
+                {
+                    output.Position = fStructOffsets[i];
+                    output.Write(BitConverter.GetBytes(fNameOffsets[i]), 0, 4); //Uncompressed size
+                    output.Write(BitConverter.GetBytes(fDataOffsets[i]), 0, 4); //Compressed size
+                    output.Write(BitConverter.GetBytes(decFileSizes[i]), 0, 4); //Uncompressed size
+                    output.Write(BitConverter.GetBytes(compFileSizes[i]), 0, 4); //Compressed size
+                }
+            }
+        }
+
+        public static int filesToPack = 0;
+        static string[]  fileNames = new string[1000];
+        static long[] decFileSizes = new long[1000];
+        static long[] compFileSizes = new long[1000];
+        static MemoryStream[] fileData = new MemoryStream[1000];
+
+        static void WalkDirectory(DirectoryInfo root, string arcFileName, string pathToPack, bool compression)
+        {
+            FileInfo[] files = null;
+            DirectoryInfo[] subDirs = null;
+
+
+            files = root.GetFiles("*.*");
+
+            if (files != null)
+            {
+                foreach (FileInfo file in files)
+                {
+                    string packFileStr = file.FullName.Substring(file.FullName.IndexOf(pathToPack));
+                    
+
+                    Console.WriteLine("{0} Packing: " + packFileStr + " into " + arcFileName, filesToPack);
+                    fileNames[filesToPack] = packFileStr;
+                    decFileSizes[filesToPack] = file.Length;
+
+                    byte[] dbytes = File.ReadAllBytes(file.FullName);
+
+                    if (dbytes.Length > 1000 && compression == true)
+                    {
+                        Console.WriteLine("Using compression");
+                        LZCompress lz = new LZCompress();
+                        lz.write(dbytes);
+                        lz.close();
+                        fileData[filesToPack] = (MemoryStream)lz.outputdata();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Not using compression");
+                        fileData[filesToPack] = new MemoryStream(dbytes);
+                    }
+
+                    compFileSizes[filesToPack] = fileData[filesToPack].Length;
+
+                    filesToPack++;
+
+                }
+
+                subDirs = root.GetDirectories();
+                foreach (DirectoryInfo dir in subDirs)
+                {
+                    WalkDirectory(dir, arcFileName, pathToPack, compression);
+                }
+
+            }
+        }
+
+        static string ReadAscii(BinaryReader input)
+        {
+            List<byte> strBytes = new List<byte>();
+            int b;
+            while ((b = input.ReadByte()) != 0x00)
+                strBytes.Add((byte)b);
+            return Encoding.ASCII.GetString(strBytes.ToArray());
+        }
+    }
+}
